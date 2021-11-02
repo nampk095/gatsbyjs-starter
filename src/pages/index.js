89469583d4148aa6e1272286db3ca3a8d778764c
MinhQuy <nampk095@gmail.com>
2021-11-02 21:14:30 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <Seo title="Trang chủ" />
+    <h1>Trang chủ</h1>
+    <p>Chào mừng bạn đến với blog demo sử dụng Gatsbyjs.</p>
+    <p>Đã xây dựng thành công.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
@@ -20,7 +20,7 @@ const IndexPage = () => (
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/contact/">Go to contact page</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
   </Layout>
